3f4130aa0356e602e963e132a2fdd89f6511f7c7
tpence06 <tpence06@hotmail.com>
2023-02-23 00:27:45 -0700

    pass tests

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2 
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 28
+let num4 = 6
+const mod = num3 % num4;
+
+const max = 20
